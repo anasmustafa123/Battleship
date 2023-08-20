e7c0730c165fa68f721e47d83d0d39ffc2c6d6a4
anasmuostafa <anasmuostafa@gmail.com>
2023-08-20 19:20:23 +0300

    update the tests for gameboard

--- src/tests/game-board.test.js
+++ src/tests/game-board.test.js
@@ -54,16 +54,54 @@ test("all sank --> 1 ship ", () => {
   expect(gameBoard1.placeShip(sPoint2, ship(4, sPoint2, "h"))).toBe(true);
   expect(gameBoard1.isAllSank()).toBe(false);
 });
-test("all sank --> 1 ship ", () => {
+test("all sank --> 1 ship & sank ", () => {
   let gameBoard1 = gameBoard();
   let sPoint2 = point(0, 5);
   expect(gameBoard1.placeShip(sPoint2, ship(4, sPoint2, "h"))).toBe(true);
+  expect(gameBoard1.receiveAttack(point(0, 5))).toEqual(4);
+  expect(gameBoard1.isAllSank()).toBe(false);
+  expect(gameBoard1.receiveAttack(point(0, 6))).toEqual(4);
+  expect(gameBoard1.isAllSank()).toBe(false);
+  expect(gameBoard1.receiveAttack(point(0, 7))).toEqual(4);
+  expect(gameBoard1.isAllSank()).toBe(false);
+  expect(gameBoard1.receiveAttack(point(0, 8))).toEqual(4);
+  expect(gameBoard1.receiveAttack(point(0, 9))).toEqual(false);
+  expect(gameBoard1.receiveAttack(point(0, 9))).toEqual(null);
+  expect(gameBoard1.isAllSank()).toBe(true);
+});
+
+
+test("all sank --> 3 ships ", () => {
+  let gameBoard1 = gameBoard();
+
+  let sPoint2 = point(0, 5); /* 'h' len=4 (0, 8) */
+  let sPoint3 = point(0, 4); /* 'v' len=5 (4, 4) */
+  let sPoint4 = point(4, 5); /* 'h' len=5(4, 9) */
+  
+  expect(gameBoard1.placeShip(sPoint2, ship(4, sPoint2, "h"))).toBe(true);
+  expect(gameBoard1.placeShip(sPoint3, ship(5, sPoint3, "v"))).toBe(true);
+  expect(gameBoard1.placeShip(sPoint4, ship(5, sPoint4, "h"))).toBe(true);
 
   expect(gameBoard1.receiveAttack(point(0, 5))).toEqual(4);
   expect(gameBoard1.receiveAttack(point(0, 6))).toEqual(4);
   expect(gameBoard1.receiveAttack(point(0, 7))).toEqual(4);
   expect(gameBoard1.receiveAttack(point(0, 8))).toEqual(4);
-  expect(gameBoard1.receiveAttack(point(0, 9))).toEqual(false);
-  expect(gameBoard1.receiveAttack(point(0, 9))).toEqual(null);
+
+  expect(gameBoard1.isAllSank()).toBe(false);
+
+  expect(gameBoard1.receiveAttack(point(0, 4))).toEqual(5);
+  expect(gameBoard1.receiveAttack(point(1, 4))).toEqual(5);
+  expect(gameBoard1.receiveAttack(point(2, 4))).toEqual(5);
+  expect(gameBoard1.receiveAttack(point(3, 4))).toEqual(5);
+  expect(gameBoard1.receiveAttack(point(4, 4))).toEqual(5);
+
+  expect(gameBoard1.isAllSank()).toBe(false);
+
+  expect(gameBoard1.receiveAttack(point(4, 5))).toEqual(5);
+  expect(gameBoard1.receiveAttack(point(4, 6))).toEqual(5);
+  expect(gameBoard1.receiveAttack(point(4, 7))).toEqual(5);
+  expect(gameBoard1.receiveAttack(point(4, 8))).toEqual(5); 
+  expect(gameBoard1.receiveAttack(point(4, 9))).toEqual(5);
+  
   expect(gameBoard1.isAllSank()).toBe(true);
 });
