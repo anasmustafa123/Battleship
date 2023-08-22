f55730baf5c42f9f4d195b7845338fda4b99e251
anasmuostafa <anasmuostafa@gmail.com>
2023-08-22 14:31:20 +0300

    add function to remove last selected ship

--- src/scripts/dom/input-ships.js
+++ src/scripts/dom/input-ships.js
@@ -1,6 +1,7 @@
 const selectShips = () => {
   let ships = [0, 0, 1, 2, 1, 1];
   let outOfShips = false;
+  let selectedShip;
   const getNextSelectedShip = () => {
     let key = 5;
     while (ships[key] == 0 && key > 0) {
@@ -9,12 +10,20 @@ const selectShips = () => {
     if (key == 0) {
       outOfShips = true;
     }
+    selectedShip = key;
     return key;
   };
   const isOutOfShips = () => {
     getNextSelectedShip();
     return outOfShips;
   };
-  return { getNextSelectedShip, isOutOfShips, ships };
+  const removeSelectedShip = () => {
+    if(ships[selectedShip] > 0){
+        ships[selectedShip]--;
+        return selectedShip;
+    };
+    return false;
+  };
+  return { getNextSelectedShip, isOutOfShips, removeSelectedShip };
 };
 export { selectShips };
--- src/tests/input-ships.test.js
+++ src/tests/input-ships.test.js
@@ -8,19 +8,24 @@ test("select a ship ---> all ships there", () => {
 
 test("select a ship ---> removing some ships", () => {
   let inputShips = selectShips();
-  inputShips.ships[5]--;
+  expect(inputShips.getNextSelectedShip()).toEqual(5);
+  expect(inputShips.removeSelectedShip()).toBe(5);
   expect(inputShips.getNextSelectedShip()).toEqual(4);
-  inputShips.ships[4]--;
-  expect(inputShips.getNextSelectedShip()).toEqual(3);
+  expect(inputShips.removeSelectedShip()).toBe(4);
   expect(inputShips.isOutOfShips()).toBe(false);
 });
 
 test("outofships --> select all ships", () => {
   let inputShips = selectShips();
-  inputShips.ships[5]--;
-  inputShips.ships[4]--;
-  inputShips.ships[3]--;
-  inputShips.ships[3]--;
-  inputShips.ships[2]--;
-  expect(inputShips.isOutOfShips()).toBe(true);
+  expect(inputShips.getNextSelectedShip()).toEqual(5);
+  expect(inputShips.removeSelectedShip()).toBe(5);
+  expect(inputShips.getNextSelectedShip()).toEqual(4);
+  expect(inputShips.removeSelectedShip()).toBe(4);
+  expect(inputShips.getNextSelectedShip()).toEqual(3);
+  expect(inputShips.removeSelectedShip()).toBe(3);
+  expect(inputShips.getNextSelectedShip()).toEqual(3);
+  expect(inputShips.removeSelectedShip()).toBe(3);
+  expect(inputShips.getNextSelectedShip()).toEqual(2);
+  expect(inputShips.removeSelectedShip()).toBe(2);
+  expect(inputShips.isOutOfShips()).toBe(true); 
 });
