f57e796997fd3912fb0b7d18b5d0aed90db0fcbf
anasmuostafa <anasmuostafa@gmail.com>
2023-08-23 15:09:56 +0300

    creates the grid with all eventlistners needed
    - hover for styling selected ship coordinates (if valid place)
    - click for styling the dropped ship (if valid place)
    
    + add the dropped ship to the player board

---
+++ src/scripts/dom/createPlayerGrid.js
@@ -0,0 +1,90 @@
+import { selectShips } from "./input-ships";
+import { ship } from "../ship";
+import { point } from "../point";
+import { show, hide } from "./popup";
+const createCoordinate = (key, playerBoard, shipsInput) => {
+  let shipCoordinate = point(parseInt(key / 10), parseInt(key % 10));
+  const coordinate = document.createElement("div");
+  coordinate.setAttribute("key", key);
+  coordinate.classList.add("grid-coordinate");
+  coordinate.addEventListener("mouseover", () => {
+    if (isAvalidCoordinate(playerBoard, shipCoordinate)) {
+      remvoeClassNameToTheShip("invalid", key, alignment, shipLength);
+      addClassNameToTheShip("hover", key, alignment, shipLength);
+    } else {
+      addClassNameToTheShip("invalid", key, alignment, shipLength);
+    }
+  });
+  coordinate.addEventListener("mouseout", () => {
+    remvoeClassNameToTheShip("hover", key, alignment, shipLength);
+  });
+  coordinate.addEventListener("click", () => {
+    if (dropTheShip(playerBoard, shipLength, shipCoordinate, alignment)) {
+      addClassNameToTheShip("ship", key, alignment, shipLength);
+      shipsInput.updateSelectedShip();
+    }
+    if (shipsInput.isOutOfShips()) {
+      show("game-start");
+    }
+  });
+  return coordinate;
+};
+
+const dropTheShip = (playerBoard, shipLength, shipCoordinate, alignment) => {
+  let newShip = ship(shipLength, shipCoordinate, alignment);
+  return playerBoard.placeShip(shipCoordinate, newShip);
+};
+const isAvalidCoordinate = (playerBoard, shipCoordinate) => {
+  let alignment = getCurrentAlignment();
+  let shipLength = shipsInput.getNextSelectedShip();
+  return playerBoard.isValidPlace(shipCoordinate, alignment, shipLength);
+};
+
+const addClassNameToTheShip = (
+  className,
+  key,
+  alignment,
+  selectedShip,
+  validPlace
+) => {
+  let arr = getShipsCoordinates(key, alignment, selectedShip, validPlace);
+  arr.map((coordinate) => {
+    if (coordinate) coordinate.classList.add(className);
+  });
+};
+const remvoeClassNameToTheShip = (
+  className,
+  key,
+  alignment,
+  selectedShip,
+  validPlace
+) => {
+  let arr = getShipsCoordinates(key, alignment, selectedShip, validPlace);
+  arr.map((coordinate) => {
+    if (coordinate) coordinate.classList.remove(className);
+  });
+};
+const getShipsCoordinates = (key, alignment, selectedShip, validPlace) => {
+  let arr = [];
+  let inc = alignment == "h" ? 1 : 10;
+  while (selectedShip--) {
+    arr.push(document.querySelector(`[key = "${key}"]`));
+    key += inc;
+  }
+  return arr;
+};
+
+const createGrid = (size, playerGameBoard) => {
+  const shipsGridContainer = document.querySelector(".ships-input-grid");
+  let shipsInput = selectShips();
+  for (let i = 0; i < size; i++) {
+    shipsGridContainer.appendChild(
+      createCoordinate(i, playerGameBoard, shipsInput)
+    );
+  }
+};
+const getCurrentAlignment = () => {
+  const rotateShipBtn = document.querySelector(".rotate-ship");
+  return rotateShipBtn.getAttribute("value");
+};
+export { createGrid };
