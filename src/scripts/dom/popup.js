81ca6c22159cb0ab3964db09cfa2afc6230bd39b
anasmuostafa <anasmuostafa@gmail.com>
2023-08-23 15:05:30 +0300

    popup object to show and hide classes

---
+++ src/scripts/dom/popup.js
@@ -0,0 +1,8 @@
+const show = (className) => {
+  document.querySelector(`.${className}`).classList.remove("hide");
+};
+const hide = (className) => {
+  document.querySelector(`.${className}`).classList.add("hide");
+};
+
+export {show, hide};
